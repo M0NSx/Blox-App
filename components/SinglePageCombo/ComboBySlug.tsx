b27f2ad6eb40a0d52796c351bab51b892acb884e
unknown <hasaphduarte@gmail.com>
2024-05-12 16:14:22 -0300

    commited

--- components/SinglePageCombo/ComboBySlug.tsx
+++ components/SinglePageCombo/ComboBySlug.tsx
@@ -5,20 +5,25 @@ import FavortiteLikeBtn from "../SinglePageCombo/FavoriteLikeBtn";
 import { Combo } from "@/lib/types";
 import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
-import { Textarea } from "@/components/ui/textarea"
-import { DifficultyBadge, RaceBadge, SpecialtyBadge, StatsBadge } from "../HtmlComponents/ComboBadges";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  DifficultyBadge,
+  RaceBadge,
+  SpecialtyBadge,
+  StatsBadge,
+} from "../HtmlComponents/ComboBadges";
 import ComboVideo from "../HtmlComponents/ComboVideo";
 import { Separator } from "../ui/separator";
-import { authOptions } from "@/app/api/auth/[...nextauth]/authOption";
-
+import { authOptions } from "../../app/api/auth/[...nextauth]/authOption";
 
 type Props = {
   combo: Combo;
-}
+};
+
 
 export default async function ComboBySlug({ combo }: Props) {
 
-  const session = await getServerSession(authOptions);
+  const session: any = await getServerSession(authOptions);
 
   const user = await prisma.user.findUnique({
     where: {
@@ -30,6 +35,20 @@ export default async function ComboBySlug({ combo }: Props) {
       name: true,
       image: true,
       createdAt: true,
+      favorites: {
+        where: {
+          user: {
+            email: session?.user?.email
+          }
+        }
+      },
+      commentLikes: {
+        where: {
+          user: {
+            email: session?.user?.email
+          }
+        }
+      }
     }
   })
 
@@ -38,82 +57,50 @@ export default async function ComboBySlug({ combo }: Props) {
       <div className="flex items-center w-full gap-2 border rounded-[8px] p-2">
         <h1 className="text-[12px]">
           You are viewing the combo{" "}
-          <span className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text font-extrabold text-transparent">{combo.combotitle}</span>
+          <span className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text font-extrabold text-transparent">
+            {combo.combotitle}
+          </span>
         </h1>
       </div>
-      <div className="sm:grid sm:grid-cols-2 sm:mx-4 sm:p-4">
-        <div>
-          <div className="flex petit:justify-center sm:grid sm:grid-cols-2 w-full gap-2 sm:gap-4">
-            <Image
-              src={combo.fightingstyle}
-              className="
-                border rounded-[8px] w-1/4 min-w-[60px] petit:w-[80px] sm:w-[130px]
-                
-              "
-              alt=""
-              width={60}
-              height={60}
-            />
-            <Image
-              src={combo.fruit}
-              className="
-                border rounded-[8px] w-1/4 min-w-[60px] petit:w-[80px] sm:w-[130px]
-                
-              "
-              alt=""
-              width={60}
-              height={60}
-            />
-            <Image
-              src={combo.sword}
-              className="
-                border rounded-[8px] w-1/4 min-w-[60px] petit:w-[80px] sm:w-[130px]
-                
-              "
-              alt=""
-              width={60}
-              height={60}
-            />
-            <Image
-              src={combo.weapon}
-              className="
-                border rounded-[8px] w-1/4 min-w-[60px] petit:w-[80px] sm:w-[130px]
-                
-              "
-              alt=""
-              width={60}
-              height={60}
-            />
-          </div>
-          <div className="flex items-center justify-between sm:hidden w-full gap-1">
-            <div className="">
-              built by
-              <HoverComboAuthor
-                authorCreatedAt={combo.authorCreatedAt.toDateString()}
-                comboAuthor={combo.author}
-                authorImage={combo.authorImage}
-              />
-            </div>
-            <FavortiteLikeBtn
-              combo={combo}
-              comboId={combo.id}
-              likeId={combo.comboLikes.find((like) => like.userId === user?.id)?.id}
-              favoriteId={combo.favorites.find((like) => like.userId === user?.id)?.id}
-              userId={user?.id}
-              pathName={""}
-              isInLikeList={!!combo.comboLikes.find((like) => like.userId === user?.id)}
-              isInFavoriteList={!!combo.favorites.find((like) => like.userId === user?.id)}
-              userEmail={user?.email}
-            />
-          </div>
-        </div>
-        <Textarea className="h-[120px] sm:h-[276.5px]" readOnly value={combo.combodescription} />
+      <div className="flex petit:justify-center w-full gap-2">
+        <Image
+          fetchPriority="high"
+          src={combo.fightingstyle}
+          className="border rounded-[8px] w-1/4 min-w-[60px] petit:w-[80px]"
+          alt=""
+          width={60}
+          height={60}
+        />
+        <Image
+          fetchPriority="high"
+          src={combo.fruit}
+          className="border rounded-[8px] w-1/4 min-w-[60px] petit:w-[80px]"
+          alt=""
+          width={60}
+          height={60}
+        />
+        <Image
+          fetchPriority="high"
+          src={combo.sword}
+          className="border rounded-[8px] w-1/4 min-w-[60px] petit:w-[80px]"
+          alt=""
+          width={60}
+          height={60}
+        />
+        <Image
+          fetchPriority="high"
+          src={combo.weapon}
+          className="border rounded-[8px] w-1/4 min-w-[60px] petit:w-[80px]"
+          alt=""
+          width={60}
+          height={60}
+        />
       </div>
-      <div className="items-center justify-between sm:justify-center hidden sm:flex w-full gap-1">
+      <div className="flex items-center justify-between sm:justify-normal w-full gap-1">
         <div className="">
           built by
           <HoverComboAuthor
-            authorCreatedAt={combo.authorCreatedAt.toDateString()}
+            authorCreatedAt={combo.authorCreatedAt}
             comboAuthor={combo.author}
             authorImage={combo.authorImage}
           />
@@ -121,15 +108,16 @@ export default async function ComboBySlug({ combo }: Props) {
         <FavortiteLikeBtn
           combo={combo}
           comboId={combo.id}
-          likeId={combo.comboLikes.find((like) => like.userId === user?.id)?.id}
-          favoriteId={combo.favorites.find((like) => like.userId === user?.id)?.id}
-          userId={user?.id}
+          likeId={combo.likes?.find((like) => like.userId === session?.user?.id)?.comboId as string}
+          isInLikeList={!!combo.likes?.find((like) => like.userId === session?.user?.id)}
+          isInFavoriteList={!!combo.favorites?.find((like) => like.userId === session?.user?.id)}
+          favoriteId={combo.favorites?.find((like) => like.userId === session?.user?.id)?.id}
+          userId={session?.user?.id}
           pathName={""}
-          isInLikeList={!!combo.comboLikes.find((like) => like.userId === user?.id)}
-          isInFavoriteList={!!combo.favorites.find((like) => like.userId === user?.id)}
           userEmail={user?.email}
         />
       </div>
+      <Textarea className="h-[120px]" readOnly value={combo.combodescription} />
       <h2 className="font-bold">Combo Properties:</h2>
       <div className="flex gap-2">
         <SpecialtyBadge specialty={combo.specialty} />
@@ -138,7 +126,7 @@ export default async function ComboBySlug({ combo }: Props) {
         <DifficultyBadge difficulty={combo.difficulty} />
       </div>
       <ComboVideo comboVideo={combo.comboVideo} />
-      <Separator className="text-black mt-2" />
+      <Separator className="text-black sm:hidden mt-2" />
     </section>
   );
 }
--- lib/actions/comboActions.ts
+++ lib/actions/comboActions.ts
@@ -1,81 +1,163 @@
 'use server';
 
-import { getServerSession } from "next-auth";
+import { getServerSession} from "next-auth";
 import prisma from "../prisma";
 import { revalidatePath } from "next/cache";
+import { authOptions } from "../../app/api/auth/[...nextauth]/authOption";
+import { Combo } from "../types";
 
-export async function addComboLike(formData: FormData) {
-    const session = await getServerSession();
-    const comboId = formData.get("comboId") as string;
-    const pathName = formData.get("pathName") as string;
+export async function addComboLike(FormData: FormData) {
+    const session = await getServerSession(authOptions);
+    const comboId = FormData.get("comboId") as string;
+    const userId = FormData.get("userId") as string;
+    const pathName = FormData.get("pathName") as string;
 
-    const data = await prisma.like.create({
-        data: {
-            combo: {
-                connect: {
-                    id: comboId,
-                },
+    try {
+        const combo = await prisma.combo.findUnique({
+            where: {
+                id: comboId,
             },
-            user: {
-                connect: {
-                    email: session?.user?.email!,
-                }
-            },
+            include: {
+                likes: true,
+            }
+        })
+
+        const isLiked = combo?.likes.find((like) => like.userId === userId); // ou like.some
+
+        //if (isLiked) {
+        //    console.log("Combo already liked / Removing like.");
+        //    await prisma.like.delete({
+        //        where: {
+        //            comboId_userId: {
+        //                comboId,
+        //                userId,
+        //            }
+        //        }
+        //    })
+        //} else {
+        //    console.log("Combo not liked / Adding like.");
+        //   
+        //}
+
+        await prisma.like.create({
+            data: {
+                comboId,
+                userId,
+            }
+        })
+
+        revalidatePath(pathName);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function removeComboLike(FormData: FormData) {
+    const session: any = await getServerSession(authOptions);
+    const comboId = FormData.get("comboId") as string;
+    const pathName = FormData.get("pathName") as string;
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email: session?.user?.email!,
         },
     });
 
-    revalidatePath(pathName);
-}
-
-export async function removeComboLike(formData: FormData) {
-    const session = await getServerSession();
-    const likeId = formData.get("likeId") as string;
-    const pathName = formData.get("pathName") as string;
-
-    const likeDetails = await prisma.like.findUnique({
-        where: { id: likeId },
-    });
-
-    if (!likeDetails) {
-        console.error("Like not found");
+    if (!user) {
+        console.error("User not found.");
         return;
     }
 
     await prisma.like.delete({
-        where: { id: likeId },
+        where: {
+            comboId_userId: {
+                comboId,
+                userId: session?.user?.id,
+            },
+        }
     });
 
     revalidatePath(pathName);
 }
 
+
 export async function addFavoriteCombo(formData: FormData) {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
     const comboId = formData.get("comboId") as string;
     const pathName = formData.get("pathName") as string;
 
-    const data = await prisma.favorite.create({
-        data: {
-            combo: {
-                connect: {
-                    id: comboId,
-                },
-            },
-            user: {
-                connect: {
-                    email: session?.user?.email!,
-                }
-            },
+    const user = await prisma.user.findUnique({
+        where: {
+            email: session?.user?.email!,
+        },
+        include: {
+            favorites: true,
+        }
+    });
+
+    if (!user) {
+        console.error("User not found.");
+        return;
+    }
+
+    const combo = await prisma.combo.findUnique({
+        where: {
+            id: comboId,
+        },
+        include: {
+            favorites: true,
+            likes: true,
         }
     })
 
-    revalidatePath(pathName);
+    if (!combo) {
+        console.error("Combo not found.");
+        return;
+    }
+
+    const isFavorite = user.favorites.some((favorite) => favorite.comboId === comboId);
+
+    if (isFavorite) {
+        console.log("Combo already favorite.");
+        return;
+    } else {
+        await prisma.favorite.create({
+            data: {
+                user: {
+                    connect: {
+                        email: session?.user?.email!,
+                    },
+                },
+                combo: {
+                    connect: {
+                        id: comboId,
+                    },
+                },
+            },
+        });
+
+        revalidatePath(pathName);
+    }
 }
 
 export async function removeFavoriteCombo(formData: FormData) {
+    const session = await getServerSession(authOptions);
+
     const favoriteId = formData.get("favoriteId") as string;
     const pathName = formData.get("pathName") as string;
 
-    const data = await prisma.favorite.delete({
+    const user = await prisma.user.findUnique({
+        where: {
+            email: session?.user?.email!,
+        },
+    });
+
+    if (!user) {
+        console.error("User not found.");
+        return;
+    }
+
+    await prisma.favorite.delete({
         where: { id: favoriteId },
     })
 
@@ -94,7 +176,7 @@ export async function deleteCombo(formData: FormData) {
 }
 
 export async function getSlugCombo(slug: string) {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
 
     const data = await prisma.combo.findUnique({
         where: { slug: slug },
@@ -155,9 +237,8 @@ export async function getSlugCombo(slug: string) {
                     createdAt: true,
                 }
             },
-            comboLikes: {
+            likes: {
                 select: {
-                    id: true,
                     comboId: true,
                     userId: true,
                     createdAt: true,
